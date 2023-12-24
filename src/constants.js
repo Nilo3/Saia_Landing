86fc686cd4c30e1d1b75996e5eddf9403c019738
unknown <nilo.echezarreta@gmail.com>
2023-12-24 16:35:30 -0300

    tenemos el home

---
+++ src/constants.js
@@ -0,0 +1,25 @@
+const links = [
+    {
+      id: 1,
+      link: "Counters",
+    },
+    {
+      id: 2,
+      link: "Trivia",
+    },
+    {
+      id: 3,
+      link: "Ranking",
+    },
+    {
+      id: 4,
+      link: "Contactanos",
+    },
+    {
+      id: 5,
+      link: "Registro",
+    }
+  ];
+
+
+  export default links
--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 import "./index.css";
-
-
+import Home from './components/Home/Home';
+import NavBar from './components/NavBar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
  <BrowserRouter>
+  <NavBar />
     <Routes>
-      <Route path="/" element = {<App />}></Route>
+      <Route path="/" element = {<Home />}></Route>
     </Routes>
  </BrowserRouter>
 );
