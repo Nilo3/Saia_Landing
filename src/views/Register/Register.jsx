244768b22b088bb9c9c19fa97dd12d5e2061f88e
unknown <nilo.echezarreta@gmail.com>
2023-12-24 18:47:05 -0300

    register in progress

--- src/views/Register/Register.jsx
+++ src/views/Register/Register.jsx
@@ -28,41 +28,37 @@ const Register = () => {
     }
 
     return (
-        <section className="flex md:justify-start items-center w-full h-screen bg-center bg-cover" 
+        <section className=" md:justify-start items-center w-full h-screen bg-center bg-cover" 
         style={{
             backgroundImage: `url(${registerImage})`,
             backgroundSize: 'cover',
             backgroundPosition: 'center'
         }}        
     >   
-            <div className="flex relative  backdrop-filter-blur border-2  justify-center items-center ">
-            <div style={{border: '4px solid white', borderRadius: '10px'}} >
-                    <form action="#" onSubmit={submitHandler} >
-                        <h2 className="text-xl text-white text-center landinginfo">Registrate</h2>
-                        <div className="relative w-full  ">
-                            <input type="text" required onChange={(e) => setUser({...user, u_username: e.target.value})} />
-                            <label className="absolute top-1/2 left-1 transform -translate-y-1/2 text-white text-base pointer-events-none transition-all duration-200 font-bold" htmlFor="#">Nombre</label>
+            <div className="flex  border-2 justify-center items-center text-center w-full h-full ">
+            <div style={{border: '4px solid white', borderRadius: '10px'}} className="items-center text-center">                     
+            <form action="#" className="mt-4 mb-4" onSubmit={submitHandler} >
+                <h2 className="text-2xl mb-8 text-white  text-center landinginfo">Registrate</h2>
+                  <div className=" w-full ">
+                    <input placeholder="Nombre" className="text-center mb-2" type="text" required onChange={(e) => setUser({...user, u_username: e.target.value})} />
+                  </div>
+                <div className="inputcontainer">
+                <MdEmail className="fill-white" />
+                <input className="text-center mb-2" placeholder="Email" type="email" required onChange={(e) => setUser({...user, u_email: e.target.value})} />
+        r        </div>
+                        <div className="inputcontainer">
+                            <AiFillLock className="fill-white" />
+                            <input placeholder="Password" className="text-center mb-2" type="password" required onChange={(e) => setUser({...user, u_password: e.target.value})} />
                         </div>
                         <div className="inputcontainer">
-                            <MdEmail className="item" />
-                            <input type="email" required onChange={(e) => setUser({...user, u_email: e.target.value})} />
-                            <label className="label" htmlFor="#">Email</label>
-                        </div>
-                        <div className="inputcontainer">
-                            <AiFillLock className="item" />
-                            <input type="password" required onChange={(e) => setUser({...user, u_password: e.target.value})} />
-                            <label className="label" htmlFor="#">Contraseña</label>
-                        </div>
-                        <div className="inputcontainer">
-                            <MdOutlineMessage className="item" />
-                            <input type="text" required onChange={(e) => setUser({...user, u_referralCode: e.target.value})} />
-                            <label className="label" htmlFor="#">Codigo de Referido</label>
+                            <MdOutlineMessage className="fill-white" />
+                            <input className='text-white text-center mb-2' placeholder="Codigo de Referido"  type="text" required onChange={(e) => setUser({...user, u_referralCode: e.target.value})} />
                         </div>
 
-                        <button type="submit" className="btn">Registrarse</button>
+                        <button type="submit" className="text-white text-2xl">Registrarse</button>
                     </form>
                     <div>
-                        <div className="register">
+                        <div className="text-white text-l ">
                             <p>Ya tengo Cuenta <a href="/login">Iniciar Sesion</a> </p>
                         </div>
                     </div>
