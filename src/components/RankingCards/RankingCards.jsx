93802b3ec98a6918d5c17f0afd8a2bd737639f3c
Nilo Echezarreta <nilo.echezarreta@gmail.com>
2023-12-27 00:52:11 -0300

    Update RankingCards.jsx

--- src/components/RankingCards/RankingCards.jsx
+++ src/components/RankingCards/RankingCards.jsx
@@ -5,8 +5,8 @@ const RankingCards = ({id, photo, name, level, xp}) => {
                 <img className="ml-4 rounded-full h-6 w-6 md:h-24 md:w-24" src={photo} alt="userphoto" />
                 <div className="flex flex-row justify-between ml-12">
                     <p className="text-l ml-12  md:text-3xl text-center landinginfo">{name.toUpperCase()}</p>
-                    <p className="text-l ml-24 md:text-3xl text-center landinginfo">NIVEL: {level}</p>
-                    <p className="text-l ml-24  md:text-3xl text-center landinginfo">XP: {xp}</p>
+                    <p className="text-l ml-6 md:text-3xl text-center landinginfo">NIVEL: {level}</p>
+                    <p className="text-l ml-6  md:text-3xl text-center landinginfo">XP: {xp}</p>
                 </div>
             </div>
         </div>
