3c1434eb7123a6cd18359df34afc80cd5a635266
Nilo Echezarreta <nilo.echezarreta@gmail.com>
2024-04-05 21:53:37 -0300

    pestaña equipo

--- src/views/Team/Team.jsx
+++ src/views/Team/Team.jsx
@@ -2,23 +2,30 @@ import React from 'react';
 import teamData from '../../assets/members/team.json';
 
 const Team = () => {
-    return (
-      <div className=''>
-        <div className="opacity-10 ellipsis z-10"></div>
-        <div className="ellipsis opacity-30 ellipsis-purple z-10"></div>
-        <div className="opacity-100 bg-zinc-900 w-[75%] mx-auto mb-48 rounded-2xl z-20 mt-12">
-          <div className="grid grid-cols md:grid-cols-2 xl:grid-cols-3 px-2">
-            {teamData.equipo.map((member) => (
-              <div key={member.id} className="text-center ">
-                <img src={member.foto} alt={member.nombre} className="w-48 h-48 mx-auto rounded-full mt-4" width={0} height={0} />
-                <p className="text-2xl font-extrabold md:mt-2 text-white ">{member.nombre}</p>
-                <p className="mt-2 text-white mb-12">{member.posicion}</p>
+  return (
+    <div className=''>
+      <div className="opacity-10 ellipsis z-10"></div>
+      <div className="ellipsis opacity-30 ellipsis-purple z-10"></div>
+      <div className="bg-zinc-900 w-[50%] md:w-[30%] md:text-xl xl:text-3xl mx-auto rounded-t-2xl z-20 mt-10 text-white text-xl font-bold flex justify-center items-center h-10 md:h-14">
+        Equipo
+      </div>
+      <div className="opacity-100 bg-zinc-900 w-[75%] md:w-[90%] lg:w-[80%] xl:w-[65%] mx-auto mb-48 rounded-2xl rounded-br-2xl rounded-bl-2xl z-20 ">
+        <div className="grid grid-cols md:grid-cols-2 px-2 lg:ml-12 2xl:ml-60 ">
+          {teamData.equipo.map((member) => (
+            <div className='flex flex-col md:flex-row mt-6'>
+              <div key={member.id} className="text-center">
+                <img src={member.foto} alt={member.nombre} className="w-48 h-48 xl:w-64 xl:h-64 mx-auto rounded-full border-2 border-white mt-4 md:mb-12 " width={0} height={0} />
               </div>
-            ))}
-          </div>
+              <div className='flex flex-col justify-center items-center md:items-start  ml-6 md:max-w-[100px] lg:max-w-[150px] 2xl:max-w-[400px] md:ml-4 '>
+                <p className="text-xl font-extrabold md:mt-2 text-white mt-4 xl:text-2xl 2xl:text-4xl ">{member.nombre}</p>
+                <p className="mt-2 text-white mb-4 xl:text-xl 2xl:text-2xl">{member.posicion}</p>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default Team;
