053b4beb57a84549e12ee54bf22246d5d4c69b8e
Nilo Echezarreta <nilo.echezarreta@gmail.com>
2024-04-11 21:30:55 -0300

    cambio

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -7,7 +7,7 @@ const Banner = () => {
             <div className='h-[70vh] w-full bg-center md:bg-top' > 
             <div className="flex flex-col md:flex-row items-center justify-center h-full">
             <div className='z-20 text-center md:text-start md:ml-6 2xl:mr-24 xl:w-96 -mt-12 md:mt-0'>
-                <h2 className='text-white text-4xl font-bold 2xl:text-4xl '>Conectando al ecosistema IA</h2>
+                <h2 className='text-white text-4xl font-bold 2xl:text-4xl '>Mostreando cambio</h2>
                 <h4 className='text-white text-sm mt-4 lg:text-xl 2xl:text-lg px-2'>La Sociedad Argentina de Inteligencia Artificial es una organización independiente, sin fines de lucro dedicada a hacer investigaciones, generar encuentros, fortalecer los nuevos medios de educación y conscientizar a la sociedad sobre el desarrollo ético de la IA.</h4>
                 <div className='flex justify-center items-center md:justify-start'>
                 <a href="https://tally.so/r/mBdlK5" className='rounded-full text-center bg-transparent mt-4 xl:mt-8 xl:py-2 md:mt-4 px-5 py-1 w-60 text-md font-semibold bg-white text-black shadow-sm ring-2 ring-inset ring-white hover:bg-black hover:text-white'>Ser parte</a>
